ac3f38613b0ceb0355f103605d1621d5c3a0ccd2
JoelR111 <117947718+JoelR111@users.noreply.github.com>
2025-05-26 22:05:10 +0100

    Basic Auth (6 Digit Code) (#2)

--- sendmsg.py
+++ sendmsg.py
@@ -1,32 +1,49 @@
 import os
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 import requests
 import json
+import secrets
+import time
 
 load_dotenv()
 
 #Get API variables from env
-APIKEY = os.getenv("WA_APIKEY")
-URL = os.getenv("WA_URL")
+APIKEY = os.getenv("AUTH_TOKEN")
+SID = os.getenv("ACC_SID")
+fromNum = os.getenv("TWILIO_PHONE")
 
-myPhoneNum = os.getenv("PHONENUM")
+#Query User's Phone Number
+#TODO add regex AND remove '0' at beginning of number.
+print("Hello, to recieve your 6 digit authentication code. Enter a phone number below:")
+enteredNum = input("(+44): ").strip().replace(" ", "")
+phoneNum = "44" + enteredNum
 
 
-headers = {
-    "Authorization": f"Bearer {APIKEY}",
-    "Content-Type": "application/json"
-}
-data = {
-    "messaging_product": "whatsapp",
-    "to": f"{myPhoneNum}",
-    "type": "text",
-    "text": {
-        "body": "Testing 123"
-    }
-}
+#Get a 6 digit auth code and start Auth timer.
+generated_AuthToken = secrets.randbelow(900000) + 100000
+timer_AuthStart = time.time()
 
-req = requests.post(URL, headers=headers, json=data)
-print("Status:", req.status_code)
-print("Response:", req.json())
+#Message Request - Twilio
+client = Client(SID, APIKEY)
 
+message = client.messages.create(
+    from_=f"{fromNum}",
+    to=f"whatsapp:+{phoneNum}",
+    body=f"Your Authentication Code is {generated_AuthToken}"
+)
+
+print("Auth Token Sent!")
+
+try:
+    #Enter Auth Token to confirm.
+    user_AuthToken = int(input("Please Enter 6 Digit Code Sent: "))
+    timer_AuthEnd = time.time() - timer_AuthStart
+
+    if not user_AuthToken == generated_AuthToken or timer_AuthEnd > 120:
+        print("Incorrect Code - You are not authorized!")
+    else:
+        print("Correct - Welcome!")
+except ValueError:
+    print("Incorrect Code - You did not enter a 6 digit code!")
